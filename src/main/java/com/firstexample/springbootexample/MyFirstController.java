fdace47d387aa1e50d711c766baf49460a455c3f
MNVDPavanKumar <pavan.1506@gmail.com>
2022-11-16 20:16:27 +0100

    my first commit

---
+++ src/main/java/com/firstexample/springbootexample/MyFirstController.java
@@ -0,0 +1,16 @@
+package com.firstexample.springbootexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("first")
+public class MyFirstController {
+
+    @GetMapping
+    public String helloWorld()
+    {
+        return "Hello, Welcome to springboot";
+    }
+}
