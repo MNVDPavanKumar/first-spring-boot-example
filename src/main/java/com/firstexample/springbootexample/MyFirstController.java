24078e69d169611371bdee5855b73739d27effd2
MNVDPavanKumar <pavan.1506@gmail.com>
2022-11-17 05:52:49 +0100

    testing commit

--- src/main/java/com/firstexample/springbootexample/MyFirstController.java
+++ src/main/java/com/firstexample/springbootexample/MyFirstController.java
@@ -11,7 +11,7 @@ public class MyFirstController {
     @GetMapping
     public String helloWorld()
     {
-        System.out.println("Testing");
+        System.out.println("Testing the controller");
         return "Hello, Welcome to springboot";
     }
 }
